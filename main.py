a14cffc696c29074a1590baa4c57c209aaec059b
jake l school acct <239911@shakopeeschools.org>
2025-11-11 10:26:22 -0600

    ask questions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import turtle as trtl
-
+# PLEASE USE camelCase
 wn = trtl.Screen()
 wn.title("trtl Boilerplate")
 wn.bgcolor("white")
@@ -7,9 +7,17 @@ wn.setup(width=800, height=600)
 
 # Create a trtl
 t = trtl.Turtle()
+t.hideturtle()
 t.shape("turtle") 
 t.color("blue")
 t.speed(3)
+
+def askQuestions():
+    global name, favoritePresident, militaryBranch
+    
+    name = wn.textinput("Question 1", "What is your name?")
+    favoritePresident = wn.textinput(f"Question 2", f"Thanks {name}, who is your favorite president?")
+    militaryBranch = wn.textinput("Question 3", "What branch of the military did you serve in?")
 
 
 # Wait for user to close wn
